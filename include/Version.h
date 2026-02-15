6359bfcd2805e84be4d8ce1f9c5853e4d45949f0
Indra Ginanjar <indraginanjar@gmail.com>
2026-02-15 14:13:22 +0700

    Rename application to 'Project Library Folder Cleanup' and executables to plf_cleanup_cli/gui

--- include/Version.h
+++ include/Version.h
@@ -4,7 +4,7 @@
 
 namespace Version {
     constexpr const char* VERSION = "0.0.1";
-    constexpr const char* APP_NAME = "Library Folder Cleanup";
+    constexpr const char* APP_NAME = "Project Library Folder Cleanup";
     
     inline std::string get_version_string() {
         return std::string(APP_NAME) + " v" + VERSION;
